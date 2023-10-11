e39d2228a44f97c0d29366d0afea44af56feba7e
Kymani Stephens <kymanist00@gmail.com>
2023-10-11 11:43:02 -0700

    get lat and lon from api call

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
 import { getWeatherData } from "./modules/utils/getWeatherData";
 
-getWeatherData() 
+
+const searchBtn = document.querySelector('#search-btn')
+
+searchBtn.addEventListener('click', getInputValue)
+
+
+
+
+// cleans up input from user to feed it into fetch url
+function cleanInput(input) {
+  const inputStr = String(input)
+  const inputLower = inputStr.toLowerCase()
+  return inputLower
+}
+
+
+// gets input value from user on btn click
+function getInputValue() {
+  const value = document.querySelector('#search-input').value
+  const cleanVal = cleanInput(value)
+
+  const coords = getWeatherData(cleanVal)
+ 
+
+  clearInputValue() 
+
+  console.log(cleanVal)
+  console.log(coords)
+}
+
+
+function clearInputValue() {
+  const searchInput = document.querySelector('#search-input')
+  searchInput.value = ''
+}
+
+
+
+// getWeatherData('portland')
+
+
--- src/modules/utils/getWeatherData.js
+++ src/modules/utils/getWeatherData.js
@@ -5,9 +5,25 @@ const lat = '45.523064'
 const lon = '-122.676483'
 
 
-export async function getWeatherData() { 
-   fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}` , {mode: 'cors'})
-   .then(response => {
-    console.log(response)  
-   })
+
+
+
+export async function getWeatherData(city) { 
+   // const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`, {mode: 'cors'})
+   const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}}&limit=1&appid=${apiKey}`, {mode: 'cors'})
+   const data = await response.json()
+   const dataObj = await data[0]
+
+   const h1 = document.querySelector('#name')
+
+   console.log(h1)
+   console.log(dataObj) 
+
+   h1.innerText = dataObj.name
+
+   const array = [dataObj.lat, dataObj.lon]
+
+   return array
+
 }
+
