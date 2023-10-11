c75a5e27dae20c6a4cac4a95a72e09c7d1be1568
Kymani Stephens <kymanist00@gmail.com>
2023-10-11 16:23:04 -0700

    fetch data from automatic geo locating url

--- src/modules/utils/weatherService.js
+++ src/modules/utils/weatherService.js
@@ -1,19 +1,10 @@
-const apiKey = ''
+const apiKey = '' 
 
 
-export async function getCityCoordinatesAsync(city) { 
-   // const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`, {mode: 'cors'})
-   const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}}&limit=1&appid=${apiKey}`, {mode: 'cors'})
+export async function getWeatherDataAsync(city) {
+   const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`, {mode: 'cors'})
    const data = await response.json()
-   const dataObj = await data[0]
 
-   const h1 = document.querySelector('#name')
-
-   h1.innerText = dataObj.name
-
-   const array = [dataObj.lat, dataObj.lon]
-
-   return array
-
+   return data 
 }
 
