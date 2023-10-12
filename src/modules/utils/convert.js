23cc365419db801a49616ee5e969fb9392499afa
Kymani Stephens <kymanist00@gmail.com>
2023-10-11 20:27:34 -0700

    update filename

--- src/modules/utils/convert.js
+++ src/modules/utils/convert.js
@@ -1,4 +1,4 @@
-export function kToF(kelvin) {
+export function kelToFahrenheit(kelvin) {
     // Formula to convert Kelvin to Fahrenheit: (K - 273.15) * 9/5 + 32
     const fahrenheit = (kelvin - 273.15) * 9/5 + 32;
 
