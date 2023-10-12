bf0a9277c582e7c14d585a8664462e1728480f97
Kymani Stephens <kymanist00@gmail.com>
2023-10-12 14:27:37 -0700

    add weather card to display data

---
+++ src/modules/components/weatherCard.js
@@ -0,0 +1,54 @@
+export function newWeatherCard(obj) {
+
+    const container = document.querySelector('#card-container')
+
+    // create card body
+    const body = document.createElement('div')
+    body.id = 'card-body'
+
+    // create text elements
+
+    const name = createText(`${obj.name}`, 'card-name')
+    body.appendChild(name)
+
+    // create description box
+
+     const descContainer = document.createElement('div')
+     descContainer.id = 'desc-container'
+ 
+     body.appendChild(descContainer)
+
+    // finish text elements 
+
+    const fahrenheit = createText(`${obj.fahrenheit} °F`, 'card-fahrenheit')
+    body.appendChild(fahrenheit)
+
+    const feelsLike = createText(`feels like: ${obj.feelsLike} °F`, 'card-feels-like')
+    body.appendChild(feelsLike)
+
+    const humidity = createText(`humidity: ${obj.humidity}%`, 'card-humidity')
+    body.appendChild(humidity)
+
+    const windSpeed = createText(`Wind: ${obj.windSpeed} km/h`, 'card-wind')
+    body.appendChild(windSpeed)
+
+    // descriptions in the get appended to container
+
+    const weatherDesc = createText(`${obj.weatherDesc} - `, 'card-weather-desc')
+    descContainer.appendChild(weatherDesc)
+
+    const weatherDescSub = createText(` ${obj.weatherDescSub}`, 'card-weather-desc-sub')
+    descContainer.appendChild(weatherDescSub)
+
+
+    container.appendChild(body)
+}
+
+function createText(innerText, id) {
+    const textElement = document.createElement('p')
+    textElement.id = id
+    textElement.innerText = innerText
+
+    return textElement
+
+}
