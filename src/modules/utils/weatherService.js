984e395a88e224f7f8b86771da593f0d736cc8a1
Kymani Stephens <kymanist00@gmail.com>
2023-10-12 12:45:03 -0700

    add function to process weather data

--- src/modules/utils/weatherService.js
+++ src/modules/utils/weatherService.js
@@ -1,5 +1,7 @@
-const apiKey = '' 
+import { getFirst5Characters, kelToFahrenheit } from "./convert"
 
+
+const apiKey = ''
 
 export async function getWeatherDataAsync(city) {
    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`, {mode: 'cors'})
@@ -8,3 +10,23 @@ export async function getWeatherDataAsync(city) {
    return data 
 }
 
+export function processWeatherData(obj) {
+
+   const { main, wind, weather } = obj;
+
+   const fahrenheitLong = kelToFahrenheit(main.temp).toString()
+   const fahrenheit = getFirst5Characters(fahrenheitLong)
+
+   const feelsLikeLong = kelToFahrenheit(main.feels_like).toString()
+   const feelsLike = getFirst5Characters(feelsLikeLong) 
+
+   return {
+     fahrenheit,
+     feelsLike,
+     humidity: main.humidity.toString(),
+     windSpeed: wind.speed.toString(),
+     weatherDesc: weather[0].main,
+     weatherDescSub: weather[0].description,
+   };
+ }
+ 
